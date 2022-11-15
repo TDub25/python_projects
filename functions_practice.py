8169f4b87d0f507b982a377d0b63dfbff934e928
Travis Storie <storie25@outlook.com>
2022-11-14 20:38:10 -0800

    Added activity comments

--- functions_practice.py
+++ functions_practice.py
@@ -1,15 +1,27 @@
+# function named hello() that prints a greeting
+# to the user. This function should accept no 
+# arguments and returns nothing.
+
 def hello():
     print("Hello User!")
   
 hello()
 
-
+# A function named pack() that accepts three 
+# arguments, and returns a single list with the 
+# three arguments inside as list elements.
 def pack(a,b,c):
     return [a,b,c]
   
 print (pack(1,2,3))
 
-
+# A function called eat_lunch(). This function 
+# should accept a list of any length, and print 
+# out a series of strings that say "First I eat __" 
+# (the first element of the list), and "Next I eat ___" 
+# (for the following elements in the list). If the list 
+# is empty, print "My lunchbox is empty!". The function 
+# should not return anything.
 def eat_lunch(my_lst):
   if len(my_lst) == 0:
     print("My lunchbox is empty!")
