12d4d18d24430aa9fb6a03e877a341b1fb235fe6
Travis Storie <storie25@outlook.com>
2022-11-14 20:10:46 -0800

    Adjusted code for readability

--- functions_practice.py
+++ functions_practice.py
@@ -1,8 +1,14 @@
 def hello():
     print("Hello User!")
+  
+hello()
+
 
 def pack(a,b,c):
     return [a,b,c]
+  
+print (pack(1,2,3))
+
 
 def eat_lunch(my_lst):
   if len(my_lst) == 0:
@@ -10,13 +16,9 @@ def eat_lunch(my_lst):
   else:
     for i in range(len(my_lst)):
       if i == 0:
-        print(f"First I eat {my_lst[0]}")
+        print(f"First I eat my {my_lst[0]}")
       else:
-        print(f"Next I eat {my_lst[i]}")
+        print(f"Next I eat my {my_lst[i]}")
 
-hello()
-print(pack("a","b","c"))
-print(pack(1,2,3))
 eat_lunch([])
-eat_lunch(["sandwich"])
-eat_lunch(["apple","banana","sandwich","cookie"])
+eat_lunch(["sandwich","vegetable","fruit","meat","dessert"])
